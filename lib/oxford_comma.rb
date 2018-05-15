9f1b1a23b2a5eb51b8b92e9030f4aad3794d9b95
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-15 17:30:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   sized = array.size()
   if sized === 1
     array.join()
-  elsif sized == 2
+  elsif sized === 2
     "#{array[0]} and #{array[1]}"
-  elsif sized == 3
+  elsif sized === 3
     "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
     list = ""
