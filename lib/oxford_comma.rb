5399ea7e682912b8b59a6aafb780ecef7dba93b9
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-15 17:17:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,6 @@ def oxford_comma(array)
   if array.size() == 1
     array.join()
   elsif array.size() == 2
-    [ "", ].join()
-    "#{array[0]} and #{array[1]}""
+    "#{array[0]} and #{array[1]}"
   end
 end
